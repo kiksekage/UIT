2b6e5b7d3c155d79181790b7bb022601f1a3adab
Thomas Nyegaard-Signori <thomas.nyegaard.signori@gmail.com>
2019-06-11 10:57:58 +0200

    make it work baby

--- python/read_serial.py
+++ python/read_serial.py
@@ -1,5 +1,4 @@
 import serial, re, argparse
-from playsound import playsound
 from foot import Foot
 import sounddevice as sd
 import numpy  # Make sure NumPy is loaded before it is used in the callback
@@ -17,7 +16,9 @@ parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument(
     '-t', '--threshold', type=float, default=200, metavar='THRESHOLD',
     help='threshold for the force sensor values')
-parser.add_argument('-i', '--input', type=int_or_str,
+parser.add_argument('-li', '--input-left', type=int_or_str,
+                    help='input device ID or substring')
+parser.add_argument('-ri', '--input-right', type=int_or_str,
                     help='input device ID or substring')
 parser.add_argument('-o', '--output', type=int_or_str,
                     help='output device ID or substring')
@@ -30,7 +31,7 @@ parser.add_argument('-l', '--latency', type=float, help='latency in seconds')
 args = parser.parse_args()
 
 #ser = serial.Serial('/dev/ttyACM0',9600)
-ser = serial.Serial('/dev/ttyUSB0',9600)
+#ser = serial.Serial('/dev/ttyUSB0',9600)
 
 # initialize the feet
 # 14 => A0, 15 => A1, ..., 19 => A5 on the arduino
@@ -38,25 +39,31 @@ right_foot = Foot('right', 14, 16, 15)
 left_foot = Foot('left', 19, 18, 17)
 experiment = 0
 
-def callback(indata, outdata, frames, time, status):
+def callback_left(indata, outdata, frames, time, status):
 
-    
-    if numpy.max(indata) > -1 or numpy.max(indata) < 1:
-      print(numpy.argmax(indata))
+    print(indata.shape)
+    print(outdata.shape)
+    print("left")
+    outdata[:] = indata
 
-    if right_foot.lifted():
-      outdata[:] = indata
-    else:
-      outdata[:] = indata
+def callback_right(indata, outdata, frames, time, status):
+
+    print(indata.shape)
+    print(outdata.shape)
+    print("right")
+    outdata[:] = indata
 
 
 try:
-  with sd.Stream(device=(args.input, args.output),
+  with sd.Stream(device=(args.input_left, args.output),
     samplerate=args.samplerate, blocksize=args.blocksize,
     dtype=args.dtype, latency=args.latency,
-    channels=args.channels, callback=callback):
+    channels=args.channels, callback=callback_left) as a, sd.Stream(device=(args.input_right, args.output),
+    samplerate=args.samplerate, blocksize=args.blocksize,
+    dtype=args.dtype, latency=args.latency,
+    channels=args.channels, callback=callback_right) as b:
     
-    while True:
+    """ while True:
       read_serial = str(ser.readline())
       found = re.findall("([0-9]+): ([0-9]+\.?[0-9]+)", read_serial)
       for pin, v in found:
@@ -73,7 +80,7 @@ try:
           if(pin in right_foot.pins): right_foot.off(pin)
 
       state = re.findall("state: (\d+)", read_serial)
-      experiment = int(state[0])
+      experiment = int(state[0]) """
 
     print('#' * 80)
     print('press Return to quit')
